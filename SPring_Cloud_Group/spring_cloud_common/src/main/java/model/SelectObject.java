4cd725cf5a2025d82e7246bbc60a4fb1ab9c3f41
suotianbao <1q2w3e4r>
2019-04-11 19:51:53 +0800

    登陆界面修正，删除安全框架，优先完成菜单，权限，用户等界面功能

---
+++ SPring_Cloud_Group/spring_cloud_common/src/main/java/model/SelectObject.java
@@ -0,0 +1,31 @@
+package model;
+
+public class SelectObject {
+    String selectValue;
+    String selectText;
+    String selected;
+
+    public String getSelectValue() {
+        return selectValue;
+    }
+
+    public void setSelectValue(String selectValue) {
+        this.selectValue = selectValue;
+    }
+
+    public String getSelectText() {
+        return selectText;
+    }
+
+    public void setSelectText(String selectText) {
+        this.selectText = selectText;
+    }
+
+    public String getSelected() {
+        return selected;
+    }
+
+    public void setSelected(String selected) {
+        this.selected = selected;
+    }
+}

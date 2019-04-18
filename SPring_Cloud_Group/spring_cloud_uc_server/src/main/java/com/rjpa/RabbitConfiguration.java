b2ff56679dc73df71f647593f70297f2ff57f267
文艺青年 <shiou_fujisaki@hotmail.com>
2019-04-18 23:54:33 +0800

    修改系统注释方法 添加平台菜单维护功能 添加系统平台启动时扫描绑定自定义权限注解并写入权限数据表 添加系统权限访问限制 添加访问安全控制框架（springboot-security） 完成JPA自定义查询处理，添加AOP配置代码，修改共通处理（用户登陆信息，及用户权限对应菜单）， 增加公共异常类处理，为日志集中管理做准备 修改系统字典，为定时任务设定做技术准备 增加RabbitMQ，为消息中间件做服务准备

---
+++ SPring_Cloud_Group/spring_cloud_uc_server/src/main/java/com/rjpa/RabbitConfiguration.java
@@ -0,0 +1,127 @@
+package com.rjpa;
+
+import com.rabbitmq.client.AMQP;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class RabbitConfiguration {
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    public static final String FANOUT_EXCHANGE = "QUEUE_ROAUTER";
+
+    public static final String EXCHANGE_A = "my-mq-exchange_A";
+    public static final String EXCHANGE_B = "my-mq-exchange_B";
+    public static final String EXCHANGE_C = "my-mq-exchange_C";
+
+
+    public static final String QUEUE_A = "QUEUE_SMS";
+    public static final String QUEUE_B = "QUEUE_EMAIL";
+    public static final String QUEUE_C = "QUEUE_QUARTZ";
+
+    public static final String ROUTINGKEY_A = "spring-boot-routingKey_A";
+    public static final String ROUTINGKEY_B = "spring-boot-routingKey_B";
+    public static final String ROUTINGKEY_C = "spring-boot-routingKey_C";
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host, port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setPublisherConfirms(true);
+        return connectionFactory;
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    //必须是prototype类型
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        return template;
+    }
+
+
+    /**
+     * 针对消费者配置
+     * 1. 设置交换机类型
+     * 2. 将队列绑定到交换机
+     FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
+     HeadersExchange ：通过添加属性key-value匹配
+     DirectExchange:按照routingkey分发到指定队列
+     TopicExchange:多关键字匹配
+     */
+    @Bean
+    public DirectExchange defaultExchangeA() {
+        return new DirectExchange(EXCHANGE_A);
+    }
+    @Bean
+    public DirectExchange defaultExchangeB() {
+        return new DirectExchange(EXCHANGE_A);
+    }
+    @Bean
+    public DirectExchange defaultExchangeC() {
+        return new DirectExchange(EXCHANGE_A);
+    }
+    //配置fanout_exchange 绑定到交换机
+    @Bean
+    FanoutExchange fanoutExchange() {
+        return new FanoutExchange(RabbitConfiguration.FANOUT_EXCHANGE);
+    }
+    /**
+     * 获取队列A
+     * @return
+     */
+    @Bean
+    public Queue queueA() {
+        return new Queue(QUEUE_A, true); //队列持久
+    }
+    public Queue queueB() {
+        return new Queue(QUEUE_B, true); //队列持久
+    }
+    public Queue queueC() {
+        return new Queue(QUEUE_C, true); //队列持久
+    }
+
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queueA()).to(defaultExchangeA()).with(RabbitConfiguration.ROUTINGKEY_A);
+    }
+    @Bean
+    public Binding bindingB() {
+        return BindingBuilder.bind(queueB()).to(defaultExchangeB()).with(RabbitConfiguration.ROUTINGKEY_B);
+    }
+    @Bean
+    public Binding bindingC() {
+        return BindingBuilder.bind(queueC()).to(defaultExchangeC()).with(RabbitConfiguration.ROUTINGKEY_C);
+    }
+    //把所有的队列都绑定到这个交换机上去
+    @Bean
+    Binding bindingExchangeA(Queue queueA,FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(queueA).to(fanoutExchange);
+    }
+    @Bean
+    Binding bindingExchangeB(Queue queueB, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(queueB).to(fanoutExchange);
+    }
+    @Bean
+    Binding bindingExchangeC(Queue queueC, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(queueC).to(fanoutExchange);
+    }
+}
---
+++ SPring_Cloud_Group/spring_cloud_uc_server/src/main/java/com/rjpa/rabbitMq/CustomProducer.java
@@ -0,0 +1,46 @@
+package com.rjpa.rabbitMq;
+
+import com.rjpa.RabbitConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class CustomProducer implements RabbitTemplate.ConfirmCallback {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    //由于rabbitTemplate的scope属性设置为ConfigurableBeanFactory.SCOPE_PROTOTYPE，所以不能自动注入
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * 构造方法注入rabbitTemplate
+     */
+    public CustomProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+        //rabbitTemplate如果为单例的话，那回调就是最后设置的内容
+        rabbitTemplate.setConfirmCallback(this);
+    }
+
+    public void sendMsg(String content) {
+        CorrelationData correlationId = new CorrelationData(UUID.randomUUID().toString());
+        //把消息放入ROUTINGKEY_A对应的队列当中去，对应的是队列A
+        rabbitTemplate.convertAndSend(RabbitConfiguration.EXCHANGE_A, RabbitConfiguration.ROUTINGKEY_A, content, correlationId);
+    }
+
+    @Override
+    public void confirm(CorrelationData correlationData, boolean b, String s) {
+        logger.info(" 回调id:" + correlationData);
+        if (b) {
+            logger.info("消息成功消费");
+        } else {
+            logger.info("消息消费失败:" + s);
+        }
+    }
+
+    public void sendAll(String content) {
+        rabbitTemplate.convertAndSend(RabbitConfiguration.FANOUT_EXCHANGE,"", content);
+    }
+}
